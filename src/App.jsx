f735e50317e898f469bd2a14502e9cef829a7e66
mhdwahyusiregar <siregarwahyu573@gmail.com>
2024-02-24 16:47:36 +0700

    fix: management dengan useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css';
 import Homepage from './pages';
+import { GlobalContext } from './context';
+
 function App() {
+  const user = {
+    username: 'mwahyusrg',
+  };
+
   return (
     <div className="App">
-      <Homepage />
+      <GlobalContext.Provider value={user}>
+        <Homepage />
+      </GlobalContext.Provider>
     </div>
   );
 }
